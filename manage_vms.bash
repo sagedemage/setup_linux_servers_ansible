b8517178aed26aa34e7744f2ad5e53782b49e35b
Salmaan Saeed <salsaeed999@gmail.com>
2025-06-10 00:04:29 -0400

    added variables to store the virtual machine uuids

--- manage_vms.bash
+++ manage_vms.bash
@@ -1,17 +1,22 @@
 #!/bin/bash
 
+vm1_uuid="115df686-10e6-4a0a-a2ab-4dde2d77b769"
+vm2_uuid="f5f1f4b3-2e1d-4f98-89af-0105eb35a5c6"
+vm3_uuid="5d05a724-41dd-4034-b886-f2b407a36f2b"
+vm4_uuid="b6f1fd19-fe07-4c2e-b735-8df4fce3b192"
+
 case "$1" in
 	"start")
-		vboxmanage startvm "115df686-10e6-4a0a-a2ab-4dde2d77b769" --type headless
-		vboxmanage startvm "f5f1f4b3-2e1d-4f98-89af-0105eb35a5c6" --type headless
-		vboxmanage startvm "5d05a724-41dd-4034-b886-f2b407a36f2b" --type headless
-		vboxmanage startvm "b6f1fd19-fe07-4c2e-b735-8df4fce3b192" --type headless
+		vboxmanage startvm $vm1_uuid --type headless
+		vboxmanage startvm $vm2_uuid --type headless
+		vboxmanage startvm $vm3_uuid --type headless
+		vboxmanage startvm $vm4_uuid --type headless
 		;;
 	"poweroff")
-		vboxmanage controlvm "115df686-10e6-4a0a-a2ab-4dde2d77b769" poweroff
-		vboxmanage controlvm "f5f1f4b3-2e1d-4f98-89af-0105eb35a5c6" poweroff
-		vboxmanage controlvm "5d05a724-41dd-4034-b886-f2b407a36f2b" poweroff
-		vboxmanage controlvm "b6f1fd19-fe07-4c2e-b735-8df4fce3b192" poweroff
+		vboxmanage controlvm $vm1_uuid poweroff
+		vboxmanage controlvm $vm2_uuid poweroff
+		vboxmanage controlvm $vm3_uuid poweroff
+		vboxmanage controlvm $vm4_uuid poweroff
 		;;
 	"status")
 		vboxmanage list runningvms
