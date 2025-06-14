3f041f98dcb957b21b34ff3df3a294b1b112e1d8
Salmaan Saeed <salsaeed999@gmail.com>
2025-06-14 01:02:19 -0400

    added the uuid for the fifth virtual machine

--- manage_vms.bash
+++ manage_vms.bash
@@ -4,6 +4,7 @@ vm1_uuid="115df686-10e6-4a0a-a2ab-4dde2d77b769"
 vm2_uuid="f5f1f4b3-2e1d-4f98-89af-0105eb35a5c6"
 vm3_uuid="5d05a724-41dd-4034-b886-f2b407a36f2b"
 vm4_uuid="b6f1fd19-fe07-4c2e-b735-8df4fce3b192"
+vm5_uuid="eff6e85d-016e-43f7-baeb-43f2b3e21e10"
 
 case "$1" in
 	"start")
@@ -11,12 +12,14 @@ case "$1" in
 		vboxmanage startvm $vm2_uuid --type headless
 		vboxmanage startvm $vm3_uuid --type headless
 		vboxmanage startvm $vm4_uuid --type headless
+		vboxmanage startvm $vm5_uuid --type headless
 		;;
 	"poweroff")
 		vboxmanage controlvm $vm1_uuid poweroff
 		vboxmanage controlvm $vm2_uuid poweroff
 		vboxmanage controlvm $vm3_uuid poweroff
 		vboxmanage controlvm $vm4_uuid poweroff
+		vboxmanage controlvm $vm5_uuid poweroff
 		;;
 	"status")
 		vboxmanage list runningvms
